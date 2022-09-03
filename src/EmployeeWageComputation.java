fb9e0f727b196b465c424e1afb6fa01d721ae565
Indranil998 <indranilm1998@gmail.com>
2022-09-04 01:24:25 +0530

    ADDED: all project file

---
+++ src/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+
+public class EmployeeWageComputation {
+	
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("Monthly wage for employee is "+employee.monthlyWage());
+	}
+
+}
